e9035b8477f2d8b21dc31e66d28c499d6fd289b7
douglasmccuen <doug@kemosabe7.com>
2022-11-27 21:03:36 -0700

    fix sound

--- renderer/components/carousel/prespin.ts
+++ renderer/components/carousel/prespin.ts
@@ -1,10 +1,12 @@
 //import roulettewheel from './sound/roulettewheel.mp3'
 
-//const carouselSpin = new Audio(roulettewheel);
-//const play = carouselSpin.play.bind(carouselSpin);
+const roulettewheel = '/audio/roulettewheel.mp3'
 
 const prespin = (length: number) => {
-  //setTimeout(play, 100)
+  const sound = new Audio(roulettewheel)
+  const play = sound.play.bind(sound);
+  setTimeout(play, 100)
+  
   const idx = Math.round(Math.random() * length);
   return Promise.resolve(idx)
 }
